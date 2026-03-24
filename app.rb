43897f1fc1ad8f57414fcb443b9eb20c219d5776
Syosen33 <syosen33@gmail.com>
2026-03-24 14:26:16 +0900

    修正

--- app.rb
+++ app.rb
@@ -41,33 +41,34 @@ post '/callback' do
   body = request.body.read
   json = JSON.parse(body)
 
-  client += Line::Bot::Client.new { |config|
+  client = Line::Bot::Client.new do |config|
     config.channel_id = ENV['LINE_CHANNEL_ID']
     config.channel_secret = ENV['LINE_CHANNEL_SECRET']  
     config.channel_token = ENV['LINE_CHANNEL_TOKEN']
-  }
+  end
 
-  client.validate_signature(body, request.env['HTTP_X_LINE_SIGNATURE']) 
-  error 400 do
-    'Bad Request'
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
+  unless client.validate_signature(body, signature)
+    halt 400, 'Bad Request'
   end
-end
-events = client.parse_events_from(body)
-events.each  |event|
-  case event
-  when Line::Bot::Event::Message
-  openai_client = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
-  response = openai_client.chat(
-    model; "gpt-4o-mini",
-    message = { role ; "user", content: event.message['text'] }],
-    temperature: 0.7,
-  }
-  )
-  message text= response["choices"].map{|c| c["message"]["content"] }.join
-  massage = { type: 'text', text: message }
-  
-  client.reply_message(event['replyToken'], message)
+
+  events = client.parse_events_from(body)
+  events.each do |event|
+    case event
+    when Line::Bot::Event::Message
+      openai_client = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
+      response = openai_client.chat(
+        parameters: {
+          model: "gpt-4o-mini",
+          messages: [{ role: "user", content: event.message['text'] }],
+          temperature: 0.7
+        }
+      )
+      message_text = response.dig("choices", 0, "message", "content")
+      message = { type: 'text', text: message_text }
+      
+      client.reply_message(event['replyToken'], message)
     end
-    "OK!"
   end
+  "OK"
 end
